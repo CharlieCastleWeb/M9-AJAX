7ca3fad3b4fbb012801660a0cff9a2a2fb96c80c
CharlieCastleWeb <castillomartinezcarlos@gmail.com>
2021-04-22 12:55:31 +0200

    Entrega

---
+++ script.js
@@ -0,0 +1,23 @@
+var image = new Array();
+for(let i=1; i<=9; i++){
+    image[i]="img/Norris"+i+".jpg"
+}
+let size = image.length;
+
+function joke() {
+    let x = Math.floor(size*Math.random());
+    $("#randomImage").attr("display", "block");
+    $("#randomImage").attr("src", image[x]);
+    $.get("http://api.icndb.com/jokes/random", function (data, textStatus, jqXHR) {
+        console.log(data);
+        $("#joke").html(`${data.value.joke}`);
+    });
+}
+
+$(document).ready(function(){
+    joke();
+    $("#jokeBtn").click(function (e) { 
+        e.preventDefault();
+        joke();
+    });
+});
